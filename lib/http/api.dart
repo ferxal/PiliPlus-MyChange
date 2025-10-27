19a7d0e51686b9015e5969a5149c90d796d4ab66
ferxal <ferxalhhh@gmail.com>
2025-10-27 17:28:51 +0800

    refactor(ci): 优化CI配置并更新依赖
    更新Gradle内存配置至8G
    升级media_kit_libs_android_video至1.3.8
    删除不再使用的iOS和MacOS工作流
    重构Android构建流程并添加缓存
    简化release工作流并添加测试环节
    修复http/api.dart中的导入路径大小写问题

--- lib/http/api.dart
+++ lib/http/api.dart
@@ -1,4 +1,4 @@
-import 'package:piliplus/http/constants.dart';
+import 'package:PiliPlus/http/constants.dart';
 
 class Api {
   // 推荐视频
@@ -407,8 +407,7 @@ class Api {
   static const String blackLst = '/x/relation/blacks';
 
   // github 获取最新版
-  static const String latestApp =
-      'https://api.github.com/repos/ferxal/piliplus-MyChange/releases';
+  static const String latestApp = 'https://api.github.com/repos/ferxal/PiliPlus-MyChange/releases';
 
   // 多少人在看
   // https://api.bilibili.com/x/player/online/total?aid=913663681&cid=1203559746&bvid=BV1MM4y1s7NZ&ts=56427838
